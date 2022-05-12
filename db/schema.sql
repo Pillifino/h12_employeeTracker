a1c5730b3d03f4bcea02115bd0a8709eaa05d17d
Pillifino <dinodizon073100@gmail.com>
2022-05-11 19:56:28 -0700

    tables in seeds and schema.sql are working rn. Use this as a save point

--- db/schema.sql
+++ db/schema.sql
@@ -15,6 +15,7 @@ CREATE TABLE roles(
   PRIMARY KEY,
   role_id INT,
   role_description TEXT NOT NULL,
+  salary VARCHAR(100) NOT NULL,
   FOREIGN KEY (role_id) 
   REFERENCES departments(id)
   ON DELETE SET NULL
@@ -29,20 +30,3 @@ CREATE TABLE employees(
   REFERENCES departments(id)
   ON DELETE SET NULL
 );
-
--- working tables
--- CREATE TABLE departments (
---   id INT,
---   department_name VARCHAR(100)
--- );
-
--- CREATE TABLE roles (
---   id INT,
---   role_name VARCHAR(100)
--- );
-
--- CREATE TABLE employees (
---   id INT,
---   first_name VARCHAR(100)
--- );
-
--- db/seeds.sql
+++ db/seeds.sql
@@ -5,25 +5,25 @@ VALUES ("Sales"),
        ("Customer Care"),
        ("Accounting");
 
-INSERT INTO roles (role_id,role_description)
-VALUES (1, "Sales Lead"),
-       (1, "Salesperson"),
-       (2, "Lead Engineer"),
-       (2, "Software Engineer"),
-       (1, "Account Manager"),
-       (4, "Accountant"),
-       (3, "Legal Team Lead"),
-       (3, "Lawyer"),
-       (4, "Customer Service");
+INSERT INTO roles (role_id,role_description,salary)
+VALUES (1, "Sales Lead","75000"),
+       (1, "Salesperson","65000"),
+       (2, "Lead Engineer", "120000"),
+       (2, "Software Engineer", "160000"),
+       (1, "Account Manager", "110000"),
+       (4, "Accountant", "80000"),
+       (3, "Legal Team Lead", "140000"),
+       (3, "Lawyer", "120000"),
+       (4, "Customer Service", "55000");
     
 INSERT INTO employees (employee_id,first_name,last_name)
 VALUES (1, "Henry", "Armstrong"),
-       (4, "Paul","Mcartney"),
-       (3, "Eric","Clapton"),
-       (1, "Sally","Clemson"),
-       (1, "Austin","Powers"),
-       (2, "Robert","Deniro"),
-       (1, "Emily","Madding"),
-       (4, "Lawrence","Pineda"),
-       (4, "Danny","Phantom");
+       (4, "Paul", "Mcartney"),
+       (3, "Eric", "Clapton"),
+       (1, "Sally", "Clemson"),
+       (1, "Austin", "Powers"),
+       (2, "Robert", "Deniro"),
+       (1, "Emily", "Madding"),
+       (4, "Lawrence", "Pineda"),
+       (4, "Danny", "Phantom");
        
