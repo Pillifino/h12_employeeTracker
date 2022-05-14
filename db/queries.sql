4d9536feea83a67cbf2e92d46969ee04b090ec52
Pillifino <dinodizon073100@gmail.com>
2022-05-13 19:07:20 -0700

    Join tables is now working! just need to get it to show the table in node

--- db/queries.sql
+++ db/queries.sql
@@ -1,6 +1,6 @@
 -- Attempt at adding the salaries based on departments
 
--- FROM course_names;
+-- FROM company_db;
 
 -- SELECT department_id, COUNT(id) AS number_department
 -- FROM roles
@@ -11,23 +11,25 @@
 -- GROUP BY department_id;
 
 -- Attempt at joining the Roles & Department tables
--- SELECT *
--- FROM roles
--- JOIN departments ON roles.department_id = departments.id;
+SELECT roles.id as role_id, roles.department_id as department_id, roles.title as role_title,
+roles.salary as role_salary, departments.id as department_id, departments.department_name as department_name
+FROM roles
+JOIN departments ON roles.department_id = departments.id;
 
--- ################
--- ## In the result fieldset, you can:
--- ## 1. preface the field names with the table name to specify
--- ## 2. alias the original field name to something else
+################
+## In the result fieldset, you can:
+## 1. preface the field names with the table name to specify
+## 2. alias the original field name to something else
 
--- -- Attempt at joining the Employees & Department tables
--- SELECT *
--- FROM employees
--- JOIN departments ON employees.department = departments.id;
 
--- ################
--- ## In the result fieldset, you can:
--- ## 1. preface the field names with the table name to specify
--- ## 2. alias the original field name to something else
+-- -- joining the Employees & Department tables
+SELECT *
+FROM employees
+JOIN departments ON employees.role_id = departments.id;
+
+################
+## In the result fieldset, you can:
+## 1. preface the field names with the table name to specify
+## 2. alias the original field name to something else
 
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
+-- USE company_db;
+
 INSERT INTO departments (department_name)
 VALUES ("Sales"),
        ("Engineering"),
@@ -16,6 +18,7 @@ VALUES (1, "Sales Lead","75000"),
        (3, "Lawyer", "120000"),
        (4, "Customer Service", "55000");
     
+-- COPY OF EMPLOYEES INCASE I MESS UP
 INSERT INTO employees (role_id,manager_id,first_name,last_name)
 VALUES (1, 1, "Henry", "Armstrong"),
        (1, 4, "Paul", "Mcartney"),
@@ -26,4 +29,15 @@ VALUES (1, 1, "Henry", "Armstrong"),
        (3, 1, "Emily", "Madding"),
        (3, 4, "Lawrence", "Pineda"),
        (4, 4, "Danny", "Phantom");
+
+-- INSERT INTO employees (role_id,manager_id,department_id,first_name,last_name)
+-- VALUES (1, 1, 3, "Henry", "Armstrong"),
+--        (1, 4, 5, "Paul", "Mcartney"),
+--        (2, 3, 6, "Eric", "Clapton"),
+--        (2, 1, 7, "Sally", "Clemson"),
+--        (1, 1, 2, "Austin", "Powers"),
+--        (4, 2, 4, "Robert", "Deniro"),
+--        (3, 1, 8, "Emily", "Madding"),
+--        (3, 4, 3, "Lawrence", "Pineda"),
+--        (4, 4, 2, "Danny", "Phantom");
        
